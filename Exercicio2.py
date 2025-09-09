4f68906f6af075b4c6e679cebad19c15cab79ed0
AndreyCorsi <andreycorsimoguedes@gmail.com>
2025-09-09 18:28:36 -0300

    Exercicio2.py

--- Exercicio2.py
+++ Exercicio2.py
@@ -1,38 +1,43 @@
-#Sistema de Cadastro de Paciente
-#1 - Cadastrar paciente
-#2 - Mostrar paciente cadastrado
-#3 - Atender paciente
-#4 - Sair
-def menu():
-    print("\n---Sistema de Gerenciamento de Paciente")
-    print("1 - Cadastrar paciente")
-    print("2 - Mostrar paciente cadastrado")
-    print("3 - Atender paciente")
-    print("4 - Listar pacientes registrados")
-    print("5 - Sair")
-    return input("Escolha uma opção: ")
-    
-    paciente = [None]
-    
-while True:
-    opcao = menu()
-        
-    if opcao == "1":
-        paciente = input("Digite nome do paciente: ")
-        print(f"Paciente {[paciente]}cadastrado com sucesso")
-        
-    elif opcao == "2":
-        if paciente is None
-            print("Nenhum paciente registrado")
-        else:
-            print(f"Pacientes atuais {paciente} ")
-        
-    
-    
-    
-    
+#exercicio2
+#cadastrar paciente
+#remover paciente
+#mostrar pacientes cadastrados
 
+def menu():
+    print("\n--- Sistema Hospitalar ---")
+    print("1 - Cadastrar Paciente")
+    print("2 - Consultar Pacientes")
+    print("3 - Atender pacientes")
+    print("4 - Sair")
+    return input("Escolha uma opção: ")
+
+paciente = []
+
+while True:
     
+    opcao = menu()
     
+    if opcao == "1":
+       paciente.append(input("Qual nome do paciente: "))
+       print("Paciente Cadastrado com sucesso!")
+       
+    elif opcao == "2":
+        if paciente == []:
+            print("Não a pacientes na Lista!")
+        else:
+            print(f"paciente: {paciente}")
+            
+    elif opcao == "3":
+        if len (paciente) == 0:
+            print("Não a pacientes para atender!")
+        else:
+            paciente.pop()
+            print(f"Lista de pacientes a ser atendidos{paciente}: ")
     
+    elif opcao == "4":
+            print("Saindo do sistema... até mais")
+            break
     
+    else:
+
+        print("Opção Invalida! Tente Novamente")
